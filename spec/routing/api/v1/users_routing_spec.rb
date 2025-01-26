b8e827f3271dc54e93e7f476e864eb977f400e90
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-01-26 20:51:32 -0300

    tests for routes

---
+++ spec/routing/api/v1/users_routing_spec.rb
@@ -0,0 +1,14 @@
+# spec/routing/api/v1/users_routing_spec.rb
+require "rails_helper"
+
+RSpec.describe Api::V1::UsersController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(get: "/api/v1/users").to route_to("api/v1/users#index")
+    end
+
+    it "routes to #create" do
+      expect(post: "/api/v1/users").to route_to("api/v1/users#create")
+    end
+  end
+end
